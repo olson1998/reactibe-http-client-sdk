1975e99905e7223b9187dfeffc875bf82d3b00a5
MSI <aleksander.inglot98@gmail.com>
2023-12-29 07:13:42 +0100

    feature/sdk improvments

---
+++ reactive-http-model/src/main/java/com/github/olson1998/http/attribute/CommonAttributes.java
@@ -0,0 +1,13 @@
+package com.github.olson1998.http.attribute;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class CommonAttributes {
+
+    public static final String WEB_REQUEST_TIMEOUT_DURATION = "web.request.timeout-duration";
+
+    public static final String WEB_REQUEST_BODY_MAX_SIZE  = "web.request.body.max-size";
+
+    public static final String WEB_RESPONSE_BODY_MAX_SIZE  = "web.response.body.max-size";
+}
